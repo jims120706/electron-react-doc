efa9ecccfd7a7e00066f88f206b6a5d1a8bc8787
jims120706 <740767260@qq.com>
2020-09-24 23:06:17 +0800

    feat：添加键盘事件，添加input自动高亮

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ function App() {
   return (
     <div className="App container-fluid">
       <div className="row">
-        <div className="col-3 left-panel">
-          <FileSearch title="我的源文档" onFileSearch={() => {}}/>
+        <div className="col left-panel">
+          <FileSearch title="我的源文档" onFileSearch={(value) => { console.log(value) }} />
         </div>
-        <div className="col-9 bg-primary right-panel">
+        <div className="col bg-primary right-panel">
           <h1>right</h1>
         </div>
       </div>
--- src/components/FileSearch.js
+++ src/components/FileSearch.js
@@ -1,21 +1,52 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 const FileSearch = ({ title, onFileSearch }) => {
-    const [inputActive, setInputActive] = useState(false)
-    const [value, setValue] = useState('')
+	const [inputActive, setInputActive] = useState(false)
+	const [value, setValue] = useState('')
 
-    return (
-        <div className="alert alert-primary">
-            {!inputActive && <div className="d-flex justify-content-between align-items-center">
-                <span>{title}</span>
-                <button type="button" className="btn btn-primary" onClick={() => { setInputActive(true) }}>搜索</button>
-            </div>}
-            {inputActive && <div className="row">
-                <input className="form-control col-8" value={value} onChange={(e) => { setValue(e.target.value) }} />
-                <button type="button" className="btn btn-primary col-4" onClick={() => { setInputActive(false) }}>关闭</button>
-            </div>}
-        </div>
-    )
+	// 关闭搜索框
+	const closeSearch = (e) => {
+		e.preventDefault()
+		setInputActive(false)
+		setValue('')
+	}
+	// input的ref
+	let node = useRef(null)
+
+	useEffect(() => {
+		const handleInputEvent = (event) => {
+			const { keyCode } = event
+			// 出现输入框并且按下键盘enter
+			if (keyCode === 13 && inputActive) {
+				onFileSearch(value)
+			}
+			// 出现输入框并且按下键盘esc
+			else if (keyCode === 27 && inputActive) {
+				closeSearch(event)
+			}
+		}
+		document.addEventListener('keyup', handleInputEvent)
+		return () => {
+			document.removeEventListener('keyup', handleInputEvent)
+		}
+	})
+	useEffect(() => {
+		if (inputActive) {
+			node.current.focus()
+		}
+	}, [inputActive])
+	return (
+		<div className="alert alert-primary">
+			{!inputActive && <div className="d-flex justify-content-between align-items-center">
+				<span>{title}</span>
+				<button type="button" className="btn btn-primary" onClick={() => { setInputActive(true) }}>搜索</button>
+			</div>}
+			{inputActive && <div className="row">
+				<input className="form-control col-8" value={value} ref={node} onChange={(e) => { setValue(e.target.value) }} />
+				<button type="button" className="btn btn-primary col-4" onClick={closeSearch}>关闭</button>
+			</div>}
+		</div>
+	)
 }
 
 export default FileSearch
