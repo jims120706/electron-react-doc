baf8bf042c7357428571aab1ffc25c5f87d00129
jims120706 <740767260@qq.com>
2020-09-24 22:50:44 +0800

    feat：初步完成fileSearch组件

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import FileSearch from './components/FileSearch'
 
 function App() {
   return (
     <div className="App container-fluid">
       <div className="row">
-        <div className="col-3 bg-danger left-panel">
-          <h1>left</h1>
+        <div className="col-3 left-panel">
+          <FileSearch title="我的源文档" onFileSearch={() => {}}/>
         </div>
         <div className="col-9 bg-primary right-panel">
           <h1>right</h1>
